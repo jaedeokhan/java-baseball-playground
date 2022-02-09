22f17d5c5fe58dca53a44f7e09679c4645c8c2f2
jaedeokhan <hjaedeok@gmail.com>
2022-02-10 00:51:59 +0900

    [ADD] 숫자야구게임 - 단위테스트란? - main method에서 처리하는 무거운 테스트를 junit을 사용해서 해결하는   방법

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    Calculator cal;
+
+    @BeforeEach
+    public void setUp() {
+        cal = new Calculator();
+    }
+
+    @Test
+    public void 덧셈() {
+        assertEquals(7, cal.add(3, 4));
+    }
+
+    @Test
+    public void 뺄셈() {
+        assertEquals(5, cal.subtract(10, 5));
+    }
+
+    @Test
+    public void 곱셈() {
+        assertEquals(2, cal.multiply(1, 2));
+    }
+
+    @Test
+    public void 나누기() {
+        assertEquals(2, cal.divide(8, 4));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        cal = null;
+    }
+}
